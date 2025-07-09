d38984fde683f231ddac493a24e3a2172ebbdac1
SARADAK <saradakrishnaneee@gmail.com>
2025-07-09 12:39:49 +0000

    added string concat function

---
+++ SARADAK_GITHUB_NOTEBOOK_DEPLOY/common_utility.py
@@ -0,0 +1,12 @@
+def concatenate_strings_plus(str1, str2):
+  """
+  Concatenates two strings using the '+' operator.
+
+  Args:
+    str1: The first string.
+    str2: The second string.
+
+  Returns:
+    A new string that is the concatenation of str1 and str2.
+  """
+  return str1 + str2
